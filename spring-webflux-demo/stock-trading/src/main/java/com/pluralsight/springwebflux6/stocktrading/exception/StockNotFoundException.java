ab95f7c74b6b0350f69b41540ade10040ee5f89a
Prem Shankar <shankar.ranjan@gmail.com>
2025-03-27 14:28:54 +0530

    Delete Stock API

--- spring-webflux-demo/stock-trading/src/main/java/com/pluralsight/springwebflux6/stocktrading/exception/StockNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.pluralsight.springwebflux6.stocktrading.exception;
-
-public class StockNotFoundException extends RuntimeException {
-
-    public StockNotFoundException(String message) {
-        super(message);
-    }
-}
